4c26756039168e244f742b3e59a90d73c68604d3
16602724475 <1714797713@qq.com>
2023-01-27 17:56:54 +0800

    base style

---
+++ src/components/ScreenAdapter.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { reactive, onMounted } from 'vue';
+import { debounce } from 'lodash';
+
+interface Props {
+  width?: string;
+  height?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  width: '1920',
+  height: '1080',
+});
+const style = reactive({
+  width: props.width + 'px',
+  height: props.height + 'px',
+  transform: 'scale(1) translate(-50%, -50%)',
+});
+//获取缩放比例
+const getScale = () => {
+  const w = window.innerHeight / ~~props.height;
+  const h = window.innerWidth / ~~props.width;
+  return w < h ? w : h;
+};
+// 设置比例
+const setScale = () => {
+  style.transform =
+    'translateZ(0) scale(' + getScale() + ') translate(-50%, -50%)';
+};
+
+onMounted(() => {
+  setScale();
+  window.onresize = debounce(setScale, 500)
+})
+</script>
+
+<template>
+  <div class="body screen-flex" :style="style">
+    <slot></slot>
+  </div>
+</template>
+<style scoped>
+
+.screen-flex {
+  background: #09152a;
+  transform-origin: 0 0;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+}
+</style>
