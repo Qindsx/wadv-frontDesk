bbeaf8308d54a169039529243e08ac6165a329d8
16602724475 <1714797713@qq.com>
2023-03-07 16:46:41 +0800

    change env

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    cors: true,
+  },
   plugins: [vue(), AutoImport({ imports: ['vue'] })],
 
   optimizeDeps: {
